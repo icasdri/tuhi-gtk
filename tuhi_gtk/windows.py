079ddba71e22fc8d397e997ddebd531c15fbf0b1
icasdri <icasdri@gmail.com>
2015-06-02 09:19:00 -0400

    Spanning table layout. Doesn't work for long note names (Gtk will resize window so proporations match).

--- tuhi_gtk/windows.py
+++ tuhi_gtk/windows.py
@@ -5,6 +5,7 @@ TITLE = "Tuhi"
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title=TITLE)
+        self.set_default_size(850, 600)
 
         # Header Bar
         self.hb = Gtk.HeaderBar()
@@ -13,24 +14,23 @@ class MainWindow(Gtk.Window):
         self.set_titlebar(self.hb)
 
         # Main Layout
-        self.grid = Gtk.Table(1, 2, False)
+        self.grid = Gtk.Table(1, 4, True)
         self.add(self.grid)
 
         self.list = Gtk.ListBox()
         self.list.set_size_request(20, 20)
         self.list.set_selection_mode(Gtk.SelectionMode.SINGLE)
 
-        a = NoteRow("Test Note")
-        b = NoteRow("My little pony")
-        c = NoteRow("Canes Chicken")
-        self.list.add(a)
-        self.list.add(b)
-        self.list.add(c)
+        for s in [
+            "Test Note", "My little pony", "Canes Chicken",
+            # "Blah blah blah blah blah blah blah blah blah"
+                ]:
+            self.list.add(NoteRow(s))
 
         self.grid.attach(self.list, 0, 1, 0, 1)
 
         self.source_view = GtkSource.View()
-        self.grid.attach(self.source_view, 1, 2, 0, 1)
+        self.grid.attach(self.source_view, 1, 4, 0, 1)
 
 
 class NoteRow(Gtk.ListBoxRow):
