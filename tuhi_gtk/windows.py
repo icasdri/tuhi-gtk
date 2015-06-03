c55c895d4a9910d4242f746d896cee5b9795e7bc
icasdri <icasdri@gmail.com>
2015-06-02 22:37:00 -0400

    Main UI using Gtk.Box (instead of Gtk.Table)

--- tuhi_gtk/windows.py
+++ tuhi_gtk/windows.py
@@ -14,8 +14,10 @@ class MainWindow(Gtk.Window):
         self.set_titlebar(self.hb)
 
         # Main Layout
-        self.grid = Gtk.Table(1, 4, True)
-        self.add(self.grid)
+        # self.grid = Gtk.Table(1, 4, True)
+        # self.add(self.grid)
+        self.main_box = Gtk.Box()
+        self.add(self.main_box)
 
         self.list = Gtk.ListBox()
         self.list.set_size_request(20, 20)
@@ -23,14 +25,16 @@ class MainWindow(Gtk.Window):
 
         for s in [
             "Test Note", "My little pony", "Canes Chicken",
-            # "Blah blah blah blah blah blah blah blah blah"
+            "Blah blah blah blah blah blah blah blah blah"
                 ]:
             self.list.add(NoteRow(s))
 
-        self.grid.attach(self.list, 0, 1, 0, 1)
+        # self.grid.attach(self.list, 0, 1, 0, 1)
+        self.main_box.pack_start(self.list, expand=False, fill=True, padding=2)
 
         self.source_view = GtkSource.View()
-        self.grid.attach(self.source_view, 1, 4, 0, 1)
+        # self.grid.attach(self.source_view, 1, 4, 0, 1)
+        self.main_box.pack_end(self.source_view, expand=True, fill=True, padding=0)
 
 
 class NoteRow(Gtk.ListBoxRow):
