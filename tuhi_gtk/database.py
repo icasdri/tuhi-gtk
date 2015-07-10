88d2910799ba998dfe4743cb2f501a0329b722d2
icasdri <icasdri@gmail.com>
2015-07-09 21:01:14 -0400

    Database-backed key value store implementation.

--- tuhi_gtk/database.py
+++ tuhi_gtk/database.py
@@ -15,11 +15,14 @@
 # You should have received a copy of the GNU General Public License
 # along with tuhi-gtk.  If not, see <http://www.gnu.org/licenses/>.
 
+import os.path
 from sqlalchemy import create_engine, Column, CHAR, String, Text, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from sqlalchemy.ext.declarative import declarative_base
+from tuhi_gtk.config import DATABASE_URI
 
-engine = create_engine('sqlite:///../test.db', convert_unicode=True)
+engine = create_engine(DATABASE_URI, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -30,6 +33,61 @@ Base.query = db_session.query_property()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+def config_database():
+    if not os.path.exists(DATABASE_URI):
+        init_db()
+
+
+TYPE_LOOKUP = {
+    'int': int
+}
+
+class KeyValueStore(object):
+    def _get_object(self, key):
+        try:
+            return KeyValueModel.query.filter_by(key=key).one()
+        except NoResultFound:
+            raise KeyError(key)
+
+    def __getitem__(self, key):
+        obj = self._get_object(key)
+        try:
+            return TYPE_LOOKUP[obj.type](obj.value)
+        except KeyError:
+            return obj.value
+
+    def __contains__(self, key):
+        try:
+            self._get_object(key)
+        except KeyError:
+            return False
+        else:
+            return True
+
+    def __setitem__(self, key, value):
+        try:
+            obj = self._get_object(key)
+        except KeyError:
+            obj = KeyValueModel(key=key)
+        obj.value = str(value)
+        obj.type = type(value).__name__
+        db_session.add(obj)
+        db_session.commit()
+
+    def __delitem__(self, key):
+        obj = self._get_object(key)
+        db_session.delete(obj)
+        db_session.commit()
+
+
+kv_store = KeyValueStore()
+
+class KeyValueModel(Base):
+    __tablename__ = 'kv_store'
+    key = Column(String, primary_key=True)
+    value = Column(String)
+    type = Column(String)
 
 
 class Note(Base):
@@ -62,7 +120,8 @@ class NotesDB:
             "More and more stuff", "Hello world", "The language",
             "Blah blah blah blah blah blah blah blah blah"
         ]
-        self.notes = {i: Note(j) for i, j in enumerate(fake_note_data)}
+        self.notes = {}
+        # self.notes = {i: Note(j) for i, j in enumerate(fake_note_data)}
 
     def get_note(self, note_id):
         return self.notes[note_id]
