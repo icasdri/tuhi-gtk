786fbccac87e06ea6e2ff64ef6f985be73bf9044
icasdri <icasdri@gmail.com>
2015-07-17 17:53:20 -0400

    Controller interface for type safety and possible code reuse.

---
+++ tuhi_gtk/controllers/controller.py
@@ -0,0 +1,30 @@
+# Copyright 2015 icasdri
+#
+# This file is part of tuhi-gtk.
+#
+# tuhi-gtk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tuhi-gtk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with tuhi-gtk.  If not, see <http://www.gnu.org/licenses/>.
+
+from tuhi_gtk.app_logging import LogInjectedObject
+
+class Controller(LogInjectedObject):
+    log_prefix_tuple = ("co",)
+
+    def startup(self):
+        pass
+
+    def set_intercontroller_dependency(self):
+        pass
+
+    def shutdown(self):
+        pass
