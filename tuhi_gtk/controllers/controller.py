a449c70175b931dbadca9784b94d121deb34fca1
icasdri <icasdri@gmail.com>
2015-07-31 19:41:53 -0400

    Start on controller/signal/hierarchy structural changes.

--- tuhi_gtk/controllers/controller.py
+++ tuhi_gtk/controllers/controller.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with tuhi-gtk.  If not, see <http://www.gnu.org/licenses/>.
 
+from gi.repository import GObject
 from tuhi_gtk.app_logging import LogInjectedObject
 
 class Controller(LogInjectedObject):
@@ -47,3 +48,53 @@ class ActivatableViewController(Controller):
 
     def do_view_activate(self):
         pass
+
+
+class NewActivatableViewController(GObject.Object):
+    was_activated_before = False
+    builder = None  # Subclasses should override in constructor, if they want get_object proxy to work
+
+    def view_activate(self):
+        if not self.was_activated_before:
+            self.do_first_view_activate()
+            self.was_activated_before = True
+        else:
+            self.do_view_activate()
+
+    def do_first_view_activate(self):
+        # Initialize your own builder and widgets here (this may change to adding to a passed-in builder)
+        # Or if you have nothing to initialize. Leave this unimplemented.
+        self.do_view_activate()
+
+    def do_view_activate(self):
+        pass
+
+    def get_object(self, object_id):
+        return self.builder.get_object(object_id)
+
+
+class WindowController(NewActivatableViewController):
+    def __init__(self, global_registrar):
+        self.global_r = global_registrar
+        self.subcontrollers = {}
+        self.do_init()
+
+    def do_init(self):
+        pass
+
+    def register_controller(self, name, controller):
+        self.subcontrollers[name] = controller
+
+    def get_controller(self, name):
+        return self.subcontrollers[name]
+
+
+class SubwindowInterfaceController(NewActivatableViewController):
+    def __init__(self, window_controller):
+        self.window = window_controller
+        self.global_r = self.window.global_r
+        self.do_init()
+
+    def do_init(self):
+        pass
+
