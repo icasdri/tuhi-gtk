4d3b1b7f6727559efdb694a532aae99134fbe44d
icasdri <icasdri@gmail.com>
2015-08-23 17:30:13 -0400

    Untested COMPLETE pull and push threaded with main loop intersplicing sync logic for sync_control. UI components to intercept the signals that come out of this logic will come later. Small parts of this logic are also likely missing due to it's untested nature.

--- tuhi_gtk/sync_control.py
+++ tuhi_gtk/sync_control.py
@@ -19,14 +19,13 @@ from gi.repository import GObject
 import requests, json
 import requests.exceptions
 import threading
-import time
 from tuhi_gtk.app_logging import get_log_for_prefix_tuple
 from tuhi_gtk.database import kv_store, get_current_date, \
     note_notonserver_tracker, note_content_notonserver_tracker, \
     note_store, note_content_store, Note
 from tuhi_gtk.config import SYNCSERVER_NOTES_ENDPOINT, REASON_SYNC, \
-    SYNC_ACTION_BEGIN, SYNC_ACTION_FAILURE, SYNC_ACTION_SUCCESS
-from tuhi_gtk.util import ignore_sender_function
+    SYNC_ACTION_BEGIN, SYNC_ACTION_FAILURE, SYNC_ACTION_SUCCESS, \
+    SYNC_FAILURE_FATAL, SYNC_FAILURE_CONNECTION, SYNC_FAILURE_AUTHENTICATION, SYNC_FAILURE_FINGERPRINT, SYNC_FAILURE_SSLHANDSHAKE
 
 log = get_log_for_prefix_tuple(("sync",))
 
@@ -34,16 +33,17 @@ class SyncControl(GObject.Object):
     __gsignals__ = {
         "global_sync_action": (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_STRING,)),
         # Action names: begin, success, failure
-        "sync_action_for_note_id": (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_STRING, GObject.TYPE_STRING)),
+        "sync_action_for_note": (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_PYOBJECT, GObject.TYPE_STRING)),
         # Action names: begin, success, failure
+        "sync_failure": (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_STRING, GObject.TYPE_PYOBJECT)),
+        # Failure types: fatal, connection, authentication, fingerprint, sslhandshake
+        # "Extra information" field containing any pyobject
     }
 
     def __init__(self, global_r):
         self.global_r = global_r
         self.global_r.instance_register(self)
         self.sync_lock = threading.Lock()
-        self.connect("server_comm_pull_complete", ignore_sender_function(self._process_pull_comm))
-        self.connect("server_comm_push_complete", ignore_sender_function(self._process_push_comm))
 
     def sync(self):
         lock_acquired = self.sync_lock.acquire(blocking=False)
@@ -64,7 +64,7 @@ class SyncControl(GObject.Object):
         pull_thread = threading.Thread(target=self._pull_comm, args=(after,))
         pull_thread.start()
 
-    def _pull_comm(self, after):
+    def _pull_comm(self, after):  # Runs in separate thread
         log.debug("Executing pull thread")
         if after is None:
             params = {}
@@ -106,7 +106,7 @@ class SyncControl(GObject.Object):
                     # We are talking about different instances. Conflict with server. Must change id of notonserver instance
                     log.warn("UUID conflict detected in pull data for %s, %s. Trying to overcome.", instance_name, serialized_data[pk_name])
                     old_id, new_id = model_store.rename_to_new_uuid(instance)
-                    log.debug("Attempted local rename %s --> %s", old_id, new_id)
+                    log.debug("UUID local rename %s --> %s", old_id, new_id)
                     note_notonserver_tracker.register_rename(old_id, new_id)
                     log.debug("Creating new %s instance for %s", instance_name, serialized_data[pk_name])
                     new_instance = model_store.add_new(serialized_data)
@@ -125,13 +125,13 @@ class SyncControl(GObject.Object):
         notonserver_note_contents = note_content_notonserver_tracker.get_all()
         yield True
 
-        notonserver_note_ids = {note.note_id for note in notonserver_notes}
-        notonserver_note_content_ids = {note_content.note_content_id for note_content in notonserver_note_contents}
-        affected_note_ids = notonserver_note_ids | {note_content.note_id for note_content in notonserver_note_contents}
+        notonserver_notes_dict = {note.note_id: note for note in notonserver_notes}
+        notonserver_note_contents_dict = {note_content.note_content_id: note_content for note_content in notonserver_note_contents}
+        affected_notes_dict = {note_content.note_id: note_content.note for note_content in notonserver_note_contents}.update(notonserver_notes_dict)
         yield True
 
-        for note_id in affected_note_ids:
-            self.emit("sync_action_for_note_id", note_id, SYNC_ACTION_BEGIN)
+        for note in affected_notes_dict.values():
+            self.emit("sync_action_for_note", note, SYNC_ACTION_BEGIN)
         yield True
 
         data_dict = {"notes": [n.serialize() for n in notonserver_notes],
@@ -141,78 +141,122 @@ class SyncControl(GObject.Object):
         data = json.dumps(data_dict)
         yield True
 
-        push_thread = threading.Thread(target=self._push_comm, args=(data,))
+        raw_tracker_dicts = (notonserver_notes_dict, notonserver_note_contents_dict, affected_notes_dict)
+        push_thread = threading.Thread(target=self._push_comm, args=(data, raw_tracker_dicts))
         push_thread.start()
 
-    def _emit_note_sync_action_on_all(self, all_affected, except_affected, all_sig_action, except_sig_action):
-        for note in all_affected:
-            if except_affected is None or note.note_id not in except_affected:
-                self.emit("note_sync_action", note, all_sig_action)
-            else:
-                self.emit("note_sync_action", note, except_sig_action)
-            yield True
-
-    def _push(self):
-        tried_notes = note_notonserver_tracker.get_all_as_query()
-        tried_note_contents = note_content_notonserver_tracker.get_all_as_query()
-
-        affected_notes = tried_notes.union(Note.query.join(tried_note_contents)).all()
-        self._emit_note_sync_action_on_all(affected_notes, None, SYNC_ACTION_BEGIN, None)
-
-        data_dict = {"notes": [n.serialize() for n in tried_notes.all()],
-                     "note_contents": [nc.serialize() for nc in tried_note_contents.all()]}
-        data = json.dumps(data_dict)
-
+    def _push_comm(self, data, raw_tracker_dicts):  # Runs in separate thread
         try:
             r = requests.post(self.sync_url, data, auth=self.auth)
         except requests.exceptions.ConnectionError as e:
             # TODO: Actual error handling logic for service unavailable, wrong network, etc.
             log.error("ConnectionError: %s", e)
-            self._emit_note_sync_action_on_all(affected_notes, None, SYNC_ACTION_FAILURE, None)
+            GObject.idle_add(lambda x: self._handle_push_connection_error(raw_tracker_dicts, e), GObject.PRIORITY_LOW)
             return
         else:
-            if r.status_code in (400, 401, 500):
-                # TODO: Actual error handling for Bad Request, Unauthorized, and Server Error
-                self._emit_note_sync_action_on_all(affected_notes, None, SYNC_ACTION_FAILURE, None)
-                return
+            post_push_comm_gen = self._post_push_comm(r, raw_tracker_dicts)
+            GObject.idle_add(lambda x: next(post_push_comm_gen, False), GObject.PRIORITY_LOW)
 
-            if r.status_code == 200:
-                note_notonserver_tracker.discard_all()
-                note_content_notonserver_tracker.discard_all()
-                self._emit_note_sync_action_on_all(affected_notes, None, SYNC_ACTION_SUCCESS, None)
-                return True
-
-            if r.status_code == 202:
-                response = r.json()
-
-                failed_notes = [x["note_id"] for x in response["notes"]] if "notes" in response else []
-                failed_note_contents = [x["note_content_id"] for x in response["note_contents"]] if "note_contents" in response else []
-
-                for note in tried_notes:
-                    if note.note_id in failed_notes:
-                        self.emit("note_sync_action", note, SYNC_ACTION_FAILURE)
-                    else:
-                        self.emit("note_sync_action", note, SYNC_ACTION_SUCCESS)
-
-                failed_notes_from_contents = set()
-                success_notes_from_contents = set()
-                for note_content in tried_note_contents:
-                    if note_content.note_content_id in failed_note_contents:
-                        failed_notes_from_contents.add(note_content.note)
-                    else:
-                        success_notes_from_contents.add(note_content.note)
-                for note in failed_notes_from_contents:
-                    self.emit("note_sync_action", note, SYNC_ACTION_FAILURE)
-                for note in success_notes_from_contents:
-                    self.emit("note_sync_action", note, SYNC_ACTION_SUCCESS)
-
-                note_notonserver_tracker.discard_all_but_failures(failed_notes)
-                note_content_notonserver_tracker.discard_all_but_failures(failed_note_contents)
-
-                # TODO: Actually read the error codes for each failure
-
-            print(r.json())
-
-    def _process_push_comm(self, res):
-        self.emit("sync_action", SYNC_ACTION_SUCCESS)  # need to use actual logic here for whether success or not
+    def _handle_push_connection_error(self, raw_tracker_dicts, e):
+        _, _, affected_notes_dict = raw_tracker_dicts
+        for note in affected_notes_dict.values():
+            self.emit("sync_action_for_note", note, SYNC_ACTION_FAILURE)
+        self.emit("sync_failure", SYNC_FAILURE_CONNECTION, (e, self.sync_url))
         self.sync_lock.release()
+        return False
+
+    def _post_push_comm(self, r, raw_tracker_dicts):
+        notes_tracking_dict, note_contents_tracking_dict, affected_notes_dict = raw_tracker_dicts
+
+        if r.status_code in (400, 500):  # Bad Request and Server Error
+            for note in affected_notes_dict.values():
+                self.emit("sync_action_for_note", note, SYNC_ACTION_FAILURE)
+            self.emit("global_sync_action", SYNC_ACTION_FAILURE)
+            self.emit("sync_failure", SYNC_FAILURE_FATAL, "Server responded with HTTP {}".format(r.status_code))
+            self.sync_lock.release()
+            return
+
+        if r.status_code == 401:  # Unauthorized
+            for note in affected_notes_dict.values():
+                self.emit("sync_action_for_note", note, SYNC_ACTION_FAILURE)
+            self.emit("global_sync_action", SYNC_ACTION_FAILURE)
+            self.emit("sync_failure", SYNC_FAILURE_AUTHENTICATION, None)
+            self.sync_lock.release()
+            return
+
+        if r.status_code == 200:
+            log.info("Server accepted push for all unsynced Notes and Note Contents")
+            for note in affected_notes_dict.values():
+                self.emit("sync_action_for_note", note, SYNC_ACTION_SUCCESS)
+            self.emit("global_sync_action", SYNC_ACTION_SUCCESS)
+            note_notonserver_tracker.discard_successes(notes_tracking_dict.keys())
+            note_content_notonserver_tracker.discard_successes(note_contents_tracking_dict.keys())
+            self.sync_lock.release()
+            return
+
+        if r.status_code == 202:
+            response = r.json()
+
+            try:
+                notes_in_response = response["notes"]
+                note_contents_in_response = response["note_contents"]
+            except KeyError:
+                for note in affected_notes_dict.values():
+                    self.emit("sync_action_for_note", note, SYNC_ACTION_FAILURE)
+                self.emit("global_sync_action", SYNC_ACTION_FAILURE)
+                self.emit("sync_failure", SYNC_FAILURE_FATAL, "Server responded with HTTP {}, but with malformed or missing json data.".format(r.status_code))
+                self.sync_lock.release()
+            else:
+                unknown_error_count = 0
+
+                for note_res in notes_in_response:
+                    note_id = note_res["note_id"]
+                    note = notes_tracking_dict[note_id]
+                    log.warn("Server rejected push of Note: %s. Further processing underway.", note_id)
+                    del notes_tracking_dict[note_id]
+                    del affected_notes_dict[note_id]
+                    self.emit("sync_action_for_note", note, SYNC_ACTION_FAILURE)
+
+                    if "note_id_errors" in note_res:
+                        if note_res["note_id_errors"] == 90:  # Forbidden
+                            log.warn("Server returned Forbidden for unsynced Note: {}. Most likely a UUID conflict. Attempting resolution.", note_id)
+                            old_id, new_id = note_store.rename_to_new_uuid(note)
+                            log.debug("UUID local rename %s --> %s", old_id, new_id)
+                            note_notonserver_tracker.register_rename(old_id, new_id)
+                        elif note_res["note_id_errors"] == 19:  # Conflict
+                            # TODO: decide if we want UUID conflict resolution logic here
+                            pass
+                        else:
+                            unknown_error_count += 1
+
+                for note_content_res in note_contents_in_response:
+                    note_content_id = note_content_res["note_content_id"]
+                    note_content = note_contents_tracking_dict[note_content_id]
+                    log.warn("Server rejected push of Note Content: %s. Further processing underway.", note_content_id)
+                    del note_contents_tracking_dict[note_content_id]
+                    del affected_notes_dict[note_content.note_id]
+                    self.emit("sync_action_for_note", note_content.note, SYNC_ACTION_FAILURE)
+
+                    if "note_content_id_errors" in note_content_res:
+                        if note_content_res["note_content_id_errors"] == 90:  # Forbidden
+                            log.warn("Server returned Forbidden for unsynced Note Content: {}. Most likely a UUID conflict. Attempting resolution.", note_content_id)
+                            old_id, new_id = note_content_store.rename_to_new_uuid(note_content)
+                            log.debug("UUID local rename %s --> %s", old_id, new_id)
+                            note_content_notonserver_tracker.register_rename(old_id, new_id)
+                        elif note_content_res["note_content_id_errors"] == 19:  # Conflict
+                            # TODO: decide if we want UUID conflict resolution logic here
+                            pass
+                        else:
+                            unknown_error_count += 1
+
+                for note in affected_notes_dict.values():
+                    self.emit("sync_action_for_note", note, SYNC_ACTION_SUCCESS)
+                note_notonserver_tracker.discard_successes(notes_tracking_dict.keys())
+                note_content_notonserver_tracker.discard_successes(note_contents_tracking_dict.keys())
+                self.emit("global_sync_action", SYNC_ACTION_FAILURE)
+                if unknown_error_count > 0:
+                    self.emit("sync_failure",
+                              SYNC_FAILURE_FATAL, "{} unknown error codes or formatting issues were encountered when processing"
+                                                  " server's response to an attempt to push unsynced notes.".format(unknown_error_count))
+                self.sync_lock.release()
+
