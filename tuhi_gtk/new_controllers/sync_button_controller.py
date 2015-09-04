f4071439e7b0aac531d55e5c5f8686727f0e782f
icasdri <icasdri@gmail.com>
2015-09-04 15:52:23 -0500

    Add emblem logic to sync_button

--- tuhi_gtk/new_controllers/sync_button_controller.py
+++ tuhi_gtk/new_controllers/sync_button_controller.py
@@ -15,22 +15,53 @@
 # You should have received a copy of the GNU General Public License
 # along with tuhi-gtk.  If not, see <http://www.gnu.org/licenses/>.
 
+from gi.repository import GObject
 from tuhi_gtk.app_logging import get_log_for_prefix_tuple
-from tuhi_gtk.util import ignore_all_args_function
+from tuhi_gtk.config import SYNC_ACTION_BEGIN, SYNC_ACTION_SUCCESS, SYNC_ACTION_FAILURE, EMBLEM_SHOW_TIMEOUT
+from tuhi_gtk.util import ignore_all_args_function, ignore_sender_function
 from tuhi_gtk.new_controllers import SubwindowInterfaceController
+from tuhi_gtk.feature_view_mixin import FeatureViewMixin
 
 log = get_log_for_prefix_tuple(("co", "sync_button"))
 
-class SyncButtonController(SubwindowInterfaceController):
+FEATURES = {
+    "normal_icon": ("icon_sync_notes", None, None),
+    "failed_icon": ("icon_sync_notes_failed", None, None),
+    "success_icon": ("icon_sync_notes_success", None, None),
+    "spinner": ("sync_button_spinner", lambda w: w.start(), lambda w: w.stop()),
+}
+
+class SyncButtonController(SubwindowInterfaceController, FeatureViewMixin):
     def do_init(self):
         self.window.register_controller("sync_button", self)
         self.window.connect("view-activated", ignore_all_args_function(self.init_after_window_activate))
 
     def init_after_window_activate(self):
+        self.normal_icon_reinstate_timeout_source = None
+        FeatureViewMixin.__init__(self, FEATURES, self.window.get_object("sync_button_box"), self.window)
+        self.show_feature("normal_icon")
         self.sync_button = self.window.get_object("sync_button")
         self.sync_button.connect("clicked", ignore_all_args_function(self.view_activate))
+        self.global_r.sync_control.connect("global_sync_action", ignore_sender_function(self.handle_global_sync_action))
 
     def do_view_activate(self):
         log.debug("Sync logic activated")
         self.global_r.sync_control.sync()
 
+    def handle_global_sync_action(self, action):
+        if action == SYNC_ACTION_BEGIN:
+            self.show_feature("spinner")
+            self.sync_button.set_sensitive(False)
+        elif action == SYNC_ACTION_SUCCESS:
+            self.show_feature("success_icon")
+            self.normal_icon_reinstate_timeout_source = GObject.timeout_add(EMBLEM_SHOW_TIMEOUT, ignore_all_args_function(self.reinstate_normal_icon_callback))
+            self.sync_button.set_sensitive(True)
+        elif action == SYNC_ACTION_FAILURE:
+            if self.normal_icon_reinstate_timeout_source is not None:
+                GObject.source_remove(self.normal_icon_reinstate_timeout_source)
+            self.show_feature("failed_icon")
+            self.sync_button.set_sensitive(True)
+
+    def reinstate_normal_icon_callback(self):
+        self.normal_icon_reinstate_timeout_source = None
+        self.show_feature("normal_icon")
