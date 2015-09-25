cb1f175fbea0b76f42a2e51a0465c243d25e079d
icasdri <icasdri@gmail.com>
2015-09-24 21:52:43 -0400

    Deletion confirmation button view logic. Deletion logic to come.

--- tuhi_gtk/new_controllers/trash_controller.py
+++ tuhi_gtk/new_controllers/trash_controller.py
@@ -15,13 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with tuhi-gtk.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 from tuhi_gtk.database import Note, NC_TYPE_PERMA_DELETE
 from tuhi_gtk.new_controllers import SubwindowInterfaceController
 from tuhi_gtk.config import get_ui_file
 from tuhi_gtk.app_logging import get_log_for_prefix_tuple
 from tuhi_gtk.new_controllers.tree_list_store_controller_mixin import TreeListStoreControllerMixin
-from tuhi_gtk.util import ignore_all_args_function, format_date, ignore_sender_function
+from tuhi_gtk.util import ignore_all_args_function, format_date, ignore_sender_function, property_change_function
 
 log = get_log_for_prefix_tuple(("co", "trash"))
 
@@ -29,6 +29,8 @@ MODEL_COLUMN_MAPPING = [lambda x: x.note_id, lambda x: format_date(x.date_conten
 
 
 class TrashController(SubwindowInterfaceController, TreeListStoreControllerMixin):
+    note_to_be_deleted = GObject.Property(type=object)
+
     def do_init(self):
         self.window.register_controller("trash", self)
         self.window.get_controller("options_popover").connect("view_activated_for_first_time",
@@ -57,7 +59,9 @@ class TrashController(SubwindowInterfaceController, TreeListStoreControllerMixin
 
         self.global_r.connect("note_metadata_changed", ignore_sender_function(self.handle_note_metadata_changed))
 
+        self.get_object("treeview").get_selection().connect("changed", self.handle_treeview_selection_changed)
         self.builder.connect_signals(self)
+        self.connect("notify::note-to-be-deleted", property_change_function(self.deletion_confirmation_selection_callback))
 
         self.get_object("trash_window").set_transient_for(self.window.get_object("main_window"))
         self.do_view_activate()
@@ -68,6 +72,7 @@ class TrashController(SubwindowInterfaceController, TreeListStoreControllerMixin
 
     def trash_window_closed(self, window, event):
         # TODO: Should we reconstruct this window every time?
+        self.note_to_be_deleted = None
         window.hide()
         return True  # Stop event from propagating
 
@@ -85,5 +90,35 @@ class TrashController(SubwindowInterfaceController, TreeListStoreControllerMixin
             print(note.note_id, ": ", note.title)
 
     def delete_permanently_button_clicked(self, _):
-        pass
+        _, it = self.get_object("treeview").get_selection().get_selected()
+        if it is not None:
+            note = self.get_item(it)
+            self.note_to_be_deleted = note
 
+    def deletion_confirmation_selection_callback(self, note):
+        if note is None:
+            log.debug("Removing deletion confirmation button.")
+            self.get_object("deletion_controls_box").remove(self.get_object("confirm_delete_button"))
+            self.get_object("delete_permanently_button").set_sensitive(True)
+        else:
+            log.debug("Prompting confirmation for permanent deletion of %s", note.note_id)
+            self.get_object("deletion_controls_box").pack_end(self.get_object("confirm_delete_button"), expand=False, fill=False, padding=0)
+            self.get_object("deletion_controls_box").show_all()
+            self.get_object("delete_permanently_button").set_sensitive(False)
+
+    def confirm_delete_button_clicked(self, _):
+        note = self.note_to_be_deleted
+        if note is not None:
+            log.info("Permanently deleting note %s", note.note_id)
+            self.note_to_be_deleted = None
+
+    def handle_treeview_selection_changed(self, tree_selection):
+        # Get rid of the deletion confirmation if it is present and we are now on a different note.
+        if self.note_to_be_deleted is not None:
+            _, it = tree_selection.get_selected()
+            if it is not None:
+                note = self.get_item(it)
+                if self.note_to_be_deleted != note:
+                    self.note_to_be_deleted = None
+            else:
+                self.note_to_be_deleted = None
